d6e380063d08a9965a8b1732028812b98778bf59
superlocalhost <76680360+superlocalhost@users.noreply.github.com>
2022-05-25 19:08:52 +0800

    pull commit4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
         System.out.println("master");
         System.out.println("push test!");
         System.out.println("push test2!");
+        System.out.println("push test3!");
 
     }
 }
