5b2bbcedf643a9547f449ac637c29470678d7a99
Layne <Layne@atguigu.com>
2022-05-25 16:58:49 +0800

    first commit1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * program: day-1
+ * author: 张爱斌
+ * create: 2022-05-25 16:57
+ * description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
