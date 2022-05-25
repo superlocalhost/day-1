467ef45971ff1cb4f4647299fbf29afde13662dd
Layne <Layne@atguigu.com>
2022-05-25 16:55:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Student.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * program: day-1
+ * author: 张爱斌
+ * create: 2022-05-25 16:52
+ * description:
+ **/
+public class Student {
+}
---
+++ src/main/java/com/atguigu/git/User.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * program: day-1
+ * author: 张爱斌
+ * create: 2022-05-25 16:44
+ * description:
+ **/
+public class User {
+    private Integer id;
+    private String username;
+    private String age;
+}
