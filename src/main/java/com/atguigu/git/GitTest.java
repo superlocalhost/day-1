df700a321eb2aa108ac7b50d33cf245f67ea1613
Layne <Layne@atguigu.com>
2022-05-25 19:03:29 +0800

    push commit 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("push test!");
+        System.out.println("push test2!");
 
     }
 }
